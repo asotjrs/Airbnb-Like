ca472a087bd4189b8345ff1ac909323fa6a59bd5
unknown <asotjrs@gmail.com>
2022-02-11 09:29:49 -0800

    adding the search result page

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,13 +1,28 @@
-import { View, Text, TextInput } from 'react-native';
-import React from 'react';
+import { View, Text, TextInput, FlatList } from 'react-native';
+import React, { useState } from 'react';
 import styles from './styles';
-
+import  searchResults  from "../../assets/data/search";
+import Entypo from "react-native-vector-icons/Entypo"
 const DestinationSearch = () => {
+
+  const [inputText, setInputText]=useState("");
   return (
-    <View>
+    <View style={styles.container}>
         <TextInput
         style={styles.textInput}
-        placeholder="Where are you going ?"
+        placeholder={"Where are you going ?"}
+        value={inputText}
+        onChangeText={setInputText}
+        />
+        <FlatList
+        data={searchResults}
+        renderItem={({item})=><View style={styles.row}>
+          <View style={styles.iconContainer}>
+            <Entypo name='location-pin' size={30}/>
+
+          </View>
+        <Text style={styles.locationText}>{item.description}</Text>
+        </View>}
         />
 
     </View>
--- src/screens/DestinationSearch/styles.js
+++ src/screens/DestinationSearch/styles.js
@@ -1,6 +1,35 @@
 import { StyleSheet } from "react-native"
 
 export default styles=StyleSheet.create({
+    container:{
+        margin:30
+    },
+    textInput:{
+        fontSize:20,
+        marginBottom:20
+
+    },
+    row:{
+        flexDirection:"row",
+        alignItems:"center",
+        paddingVertical:10,
+        borderBottomWidth:1,
+        borderBottomColor:"lightgrey"
+
+
+    },
+    iconContainer:{
+        backgroundColor:"lightgrey",
+        borderRadius:10,
+        padding:5,
+        marginRight:7
+
+
+    },
+    locationText:{
+
+
+    }
 
 
 
